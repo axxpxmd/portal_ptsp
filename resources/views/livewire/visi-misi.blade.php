35aa035c29e754b35b23ae202252875715a4f3f8
axxpxmd <asiphamdi@gmail.com>
2026-05-12 11:55:51 +0700

    Refactor Visi & Misi page: update layout with content box styling and remove unused CSS

--- resources/views/livewire/visi-misi.blade.php
+++ resources/views/livewire/visi-misi.blade.php
@@ -16,42 +16,9 @@
         bgImage="images/bg1.webp"
     />
 
-    <div class="vm-container">
-        <div class="vm-content">
-            <section class="vm-section vm-vision-section">
-                <span class="vm-badge">Visi</span>
-                <h2 class="vm-vision-text">
-                    "Terwujudnya Tangsel Unggul, Menuju Kota Lestari, Saling Terkoneksi, Efektif dan Efisien"
-                </h2>
-            </section>
-
-            <hr class="vm-divider">
-
-            <section class="vm-section vm-mission-section">
-                <span class="vm-badge">Misi</span>
-                <ul class="vm-mission-list">
-                    <li>
-                        <span class="vm-num">01</span>
-                        <p>Pembangunan Sumber Daya Manusia (SDM) Yang Unggul</p>
-                    </li>
-                    <li>
-                        <span class="vm-num">02</span>
-                        <p>Pembangunan Infrastruktur Yang Saling Terkoneksi</p>
-                    </li>
-                    <li>
-                        <span class="vm-num">03</span>
-                        <p>Membangun Kota Yang Lestari</p>
-                    </li>
-                    <li>
-                        <span class="vm-num">04</span>
-                        <p>Meningkatkan Ekonomi Berbasis Nilai Tambah Tinggi Di Sektor Ekonomi Kreatif</p>
-                    </li>
-                    <li>
-                        <span class="vm-num">05</span>
-                        <p>Membangun Birokrasi Yang Efektif Dan Efisien</p>
-                    </li>
-                </ul>
-            </section>
+    <div class="content-box-wrapper">
+       <div class="content-box">
+            {{--  --}}
         </div>
     </div>
 </div>
